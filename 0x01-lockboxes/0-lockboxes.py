48992b8a8931bd737b9387e36906b52ba16ab1b0
olaguru <dapoodedeyi03@gmail.com>
2024-10-20 21:51:05 +0100

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a method that determines if all boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ a method that determines if all boxes can be opened """
+
+    unlocked = {0}
+
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                stack.append(key)
+    return len(unlocked) == len(boxes)
